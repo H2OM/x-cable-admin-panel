89e8f0a060a44167608c3cccd6d5885c89f11a30
dimaz <dima.zabolotnov.02@mail.ru>
2026-06-23 17:58:35 +0300

    config

--- src/pages/products/ProductsFastActions.tsx
+++ src/pages/products/ProductsFastActions.tsx
@@ -1,32 +1,32 @@
-import {Spin, Typography} from "antd";
-import {SwapRightOutlined} from "@ant-design/icons";
-import {productsAPI} from "@api";
-import {useState} from "react";
-import LinkSearch from "@components/ui/form/LinkSearch.tsx";
+import {Typography} from "antd";
+// import {SwapRightOutlined} from "@ant-design/icons";
+// import {productsAPI} from "@api";
+// import {useState} from "react";
+// import LinkSearch from "@components/ui/form/LinkSearch.tsx";
 
 export default function ProductsFastActions() {
-    const [loading, setLoading] = useState<boolean>(false);
+    // const [loading, setLoading] = useState<boolean>(false);
 
-    const handleOneWayPairVariation = async (values: Record<string, string>) => {
-        console.log(values);return;
-        setLoading(true);
-
-        const response = await productsAPI.oneWayPairVariations();
-
-        setLoading(false);
-    };
-
-    const handlePairVariation = async () => {
-
-    };
-
-    const handleOneWayPairRelated = async (values: Record<string, string>) => {
-
-    };
-
-    const handlePairRelated = async (values: Record<string, string>) => {
-
-    };
+    // const handleOneWayPairVariation = async (values: Record<string, string>) => {
+    //     console.log(values);return;
+    //     setLoading(true);
+    //
+    //     const response = await productsAPI.oneWayPairVariations();
+    //
+    //     setLoading(false);
+    // };
+    //
+    // const handlePairVariation = async () => {
+    //
+    // };
+    //
+    // const handleOneWayPairRelated = async (values: Record<string, string>) => {
+    //
+    // };
+    //
+    // const handlePairRelated = async (values: Record<string, string>) => {
+    //
+    // };
 
 //TODO!!!
     return (
@@ -34,38 +34,38 @@ export default function ProductsFastActions() {
             <Typography.Title level={2} style={{margin: 0, marginBottom: '30px'}}>
                 Быстрые действия с товарами
             </Typography.Title>
-            {loading &&
-                <div className="load-screen"><Spin size="large" description={"Загрузка..."} /></div>
-            }
-            <LinkSearch title={"Односторонняя привязка вариации товара"}
-                        placeholder={"Введите имя, артикул или id товара"}
-                        divider={<SwapRightOutlined />}
-                        callback={productsAPI.search}
-                        loading={loading}
-                        submit={handleOneWayPairVariation}
-            />
-            <br/><br/>
-            <LinkSearch title={"Двусторонняя привязка вариации товара"}
-                        placeholder={"Введите имя, артикул или id товара"}
-                        callback={productsAPI.search}
-                        loading={loading}
-                        submit={handlePairVariation}
-            />
-            <br/><br/>
-            <LinkSearch title={"Односторонняя привязка связанного товара"}
-                        placeholder={"Введите имя, артикул или id товара"}
-                        divider={<SwapRightOutlined />}
-                        callback={productsAPI.search}
-                        loading={loading}
-                        submit={handleOneWayPairRelated}
-            />
-            <br/><br/>
-            <LinkSearch title={"Двусторонняя привязка связанного товара"}
-                        placeholder={"Введите имя, артикул или id товара"}
-                        callback={productsAPI.search}
-                        loading={loading}
-                        submit={handlePairRelated}
-            />
+            {/*{loading &&*/}
+            {/*    <div className="load-screen"><Spin size="large" description={"Загрузка..."} /></div>*/}
+            {/*}*/}
+            {/*<LinkSearch title={"Односторонняя привязка вариации товара"}*/}
+            {/*            placeholder={"Введите имя, артикул или id товара"}*/}
+            {/*            divider={<SwapRightOutlined />}*/}
+            {/*            callback={productsAPI.search}*/}
+            {/*            loading={loading}*/}
+            {/*            submit={handleOneWayPairVariation}*/}
+            {/*/>*/}
+            {/*<br/><br/>*/}
+            {/*<LinkSearch title={"Двусторонняя привязка вариации товара"}*/}
+            {/*            placeholder={"Введите имя, артикул или id товара"}*/}
+            {/*            callback={productsAPI.search}*/}
+            {/*            loading={loading}*/}
+            {/*            submit={handlePairVariation}*/}
+            {/*/>*/}
+            {/*<br/><br/>*/}
+            {/*<LinkSearch title={"Односторонняя привязка связанного товара"}*/}
+            {/*            placeholder={"Введите имя, артикул или id товара"}*/}
+            {/*            divider={<SwapRightOutlined />}*/}
+            {/*            callback={productsAPI.search}*/}
+            {/*            loading={loading}*/}
+            {/*            submit={handleOneWayPairRelated}*/}
+            {/*/>*/}
+            {/*<br/><br/>*/}
+            {/*<LinkSearch title={"Двусторонняя привязка связанного товара"}*/}
+            {/*            placeholder={"Введите имя, артикул или id товара"}*/}
+            {/*            callback={productsAPI.search}*/}
+            {/*            loading={loading}*/}
+            {/*            submit={handlePairRelated}*/}
+            {/*/>*/}
         </div>
     );
 }
